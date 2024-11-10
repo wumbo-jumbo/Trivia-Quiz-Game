ba7b8a26019adc705db47f0138250855e0db0ca5
Diego <148501504+wumbo-jumbo@users.noreply.github.com>
2024-11-10 08:36:17 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,473 @@
+const questions0 = [
+    {
+        question: "What is the latest Tyler, the Creator album?",
+        answers: [
+            {text: "Call Me If You Get Lost", correct: false},
+            {text: "IGOR", correct: false},
+            {text: "Chromakopia", correct: true},
+            {text: "Call Me If You Get Lost: The Estate Sale", correct: false},
+        ]
+    },
+    {
+        question: "What is the newest album from Tyler, the Creator (as of 2024)?",
+        answers: [
+            {text: "Flower Boy", correct: false},
+            {text: "IGOR", correct: false},
+            {text: "Call Me If You Get Lost", correct: false},
+            {text: "Chromakopia", correct: true},
+        ]
+    },
+    {
+        question: "What song is currently playing?",
+        answers: [
+            {text: '"Darling, I"', correct: true},
+            {text: '"BOYFRIEND"', correct: false},
+            {text: '"WUSYANAME"', correct: false},
+            {text: '"BEST INTEREST"', correct: false},
+        ]
+    },
+    {
+        question: "What album won Tyler his first Grammy?",
+        answers: [
+            {text: "Call Me If You Get Lost", correct: false},
+            {text: "IGOR", correct: true},
+            {text: "Flower Boy", correct: false},
+            {text: "Call Me If You Get Lost: The Estate Sale", correct: false},
+        ]
+    },
+    {
+        question: "What is my favorite Tyler, the Creator song?",
+        answers: [
+            {text: '"SWEET / I THOUGHT YOU WANTED TO DANCE"', correct: true},
+            {text: '"A BOY IS A GUN*"', correct: false},
+            {text: '"Pothole"', correct: false},
+            {text: '"HEAVEN TO ME"', correct: false},
+        ]
+    },
+    {
+        question: "What album gave Tyler his first Grammy nomination?",
+        answers: [
+            {text: "The Grinch EP", correct: false},
+            {text: "IGOR", correct: false},
+            {text: "Flower Boy", correct: true},
+            {text: "Cherry Bomb", correct: false},
+        ]
+    },
+    {
+        question: "Who is the persona of Call Me If You Get Lost?",
+        answers: [
+            {text: "Tyler Baudelaire", correct: true},
+            {text: "Big T", correct: false},
+            {text: "T Baby", correct: false},
+            {text: "Mr. Baudelaire", correct: false},
+        ]
+    },
+    {
+        question: "What is Tyler's full name?",
+        answers: [
+            {text: "Tyler Creator", correct: false},
+            {text: "Tyler Okonma", correct: false},
+            {text: "Tyler Gregory Okonma", correct: true},
+            {text: "Tyler Smith Okonma", correct: false},
+        ]
+    },
+    {
+        question: "What group was Tyler a part of?",
+        answers: [
+            {text: "Cactus Jack", correct: false},
+            {text: "The Internet", correct: false},
+            {text: "Odd Future", correct: true},
+            {text: "Silk Sonic", correct: false},
+        ]
+    },
+    {
+        question: "What car is referenced in LUMBERJACK?",
+        answers: [
+            {text: "Cadillac", correct: false},
+            {text: "Porshe", correct: false},
+            {text: "Rolls Royce", correct: true},
+            {text: "Bentley", correct: false},
+        ]
+    },
+    {
+        question: "Flower Boy brought many featured artists, excluding: ",
+        answers: [
+            {text: "Rex Orange County", correct: false},
+            {text: "Kali Uchis", correct: false},
+            {text: "Omar Apollo", correct: true},
+            {text: "A$AP Rocky", correct: false},
+        ]
+    },
+    {
+        question: "How many Grammy awards does Tyler have as of 2024?",
+        answers: [
+            {text: "2", correct: true},
+            {text: "1", correct: false},
+            {text: "3", correct: false},
+            {text: "4", correct: false},
+        ]
+    },
+    {
+        question: "What is the name of Tyler's annual music festival?",
+        answers: [
+            {text: "OFWGKTA Fest", correct: false},
+            {text: "Camp Flog Gnaw", correct: true},
+            {text: "Wolf Gang Fest", correct: false},
+            {text: "Golf Wang Fest", correct: false},
+        ]
+    },
+    {
+        question: "Where is Tyler from?",
+        answers: [
+            {text: "New York", correct: false},
+            {text: "Chicago", correct: false},
+            {text: "California", correct: true},
+            {text: "Boston", correct: false},
+        ]
+    },
+    {
+        question: "When did Tyler found Odd Future?",
+        answers: [
+            {text: "2009", correct: false},
+            {text: "2005", correct: false},
+            {text: "2007", correct: true},
+            {text: "2006", correct: false},
+        ]
+    },
+    {
+        question: "What is the name of the Odd Future TV show?",
+        answers: [
+            {text: "Incredible Crew", correct: false},
+            {text: "Wild n' Out", correct: false},
+            {text: "Loiter Squad", correct: true},
+            {text: "Nuts + Bolts", correct: false},
+        ]
+    },
+    {
+        question: "What was Tyler's debut studio album?",
+        answers: [
+            {text: "Wolf", correct: false},
+            {text: "Flower Boy", correct: false},
+            {text: "Goblin", correct: true},
+            {text: "Cherry Bomb", correct: false},
+        ]
+    },
+    {
+        question: "What game features Tyler's voice?",
+        answers: [
+            {text: "Fortnite", correct: false},
+            {text: "Control", correct: false},
+            {text: "GTA V", correct: true},
+            {text: "GTA IV", correct: false},
+        ]
+    },
+    {
+        question: "What cartoon featured Tyler's voice?",
+        answers: [
+            {text: "Adventure Time", correct: false},
+            {text: "Gravity Falls", correct: false},
+            {text: "Regular Show", correct: true},
+            {text: "Phineas and Ferb", correct: false},
+        ]
+    },
+    {
+        question: "What was the first single off Chromakopia?",
+        answers: [
+            {text: "Noid", correct: true},
+            {text: "Thought I Was Dead", correct: false},
+            {text: "St. Chroma", correct: false},
+            {text: "Sticky", correct: false},
+        ]
+    },
+    {
+        question: "What Japanese artist is sampled in IGOR?",
+        answers: [
+            {text: "Mariya Takeuchi", correct: false},
+            {text: "Tatsuro Yamashita", correct: true},
+            {text: "Masayoshi Takanaka", correct: false},
+            {text: "Casiopea", correct: false},
+        ]
+    },
+    {
+        question: "Which album has a deluxe version?",
+        answers: [
+            {text: "Flower Boy", correct: false},
+            {text: "IGOR", correct: false},
+            {text: "Call Me If You Get Lost", correct: true},
+            {text: "Cherry Bomb", correct: false},
+        ]
+    },
+    {
+        question: "Which album featured DJ Drama?",
+        answers: [
+            {text: "Flower Boy", correct: false},
+            {text: "Wolf", correct: false},
+            {text: "Call Me If You Get Lost", correct: true},
+            {text: "Cherry Bomb", correct: false},
+        ]
+    },
+    {
+        question: "Which album features his mother throughout?",
+        answers: [
+            {text: "Wolf", correct: false},
+            {text: "Chromakopia", correct: true},
+            {text: "Call Me If You Get Lost", correct: false},
+            {text: "Cherry Bomb", correct: false},
+        ]
+    },
+    {
+        question: "Which movie did Tyler make music for?",
+        answers: [
+            {text: "The Grinch", correct: true},
+            {text: "Despicable Me 4", correct: false},
+            {text: "Minions", correct: false},
+            {text: "Despicable Me 3", correct: false},
+        ]
+    },
+    {
+        question: "Where in Cali is Tyler from?",
+        answers: [
+            {text: "Hawthorne", correct: true},
+            {text: "West Hollywood", correct: false},
+            {text: "Century City", correct: false},
+            {text: "Santa Monica", correct: false},
+        ]
+    },
+    {
+        question: "What was Tyler's solo mixtape called?",
+        answers: [
+            {text: "Bastard", correct: true},
+            {text: "Goblin", correct: false},
+            {text: "Wolf", correct: false},
+            {text: "Odd Future", correct: false},
+        ]
+    },
+    {
+        question: "What was the first new song introduced for the Estate Sale?",
+        answers: [
+            {text: "DOGTOOTH", correct: true},
+            {text: "WUSYANAME", correct: false},
+            {text: "HEAVEN TO ME", correct: false},
+            {text: "STUTNTMAN", correct: false},
+        ]
+    },
+    {
+        question: "What is the name of Tyler's perfume?",
+        answers: [
+            {text: "Le Fleur", correct: true},
+            {text: "Odd Fleur", correct: false},
+            {text: "Wolf Fleur", correct: false},
+            {text: "Wolf le Fleur", correct: false},
+        ]
+    },
+];
+
+//shuffles the order of the questions and the order of the answers
+function shuffleArray(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1)); // Random index
+        [arr[i], arr[j]] = [arr[j], arr[i]]; // Swap elements
+    }
+}
+function randomSortArrays(nestedArray) {
+    // Step 1: Shuffle the outer array
+    shuffleArray(nestedArray);
+
+    // Step 2: Shuffle each inner array
+    nestedArray.forEach(innerArray => shuffleArray(innerArray.answers));
+
+    return nestedArray;
+}
+
+
+let questions = randomSortArrays(questions0).slice(0,10); //select how many total questions
+
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+const correctSound = document.getElementById("correctSound");
+correctSound.volume = .45;
+const incorrectSound = document.getElementById("incorrectSound");
+incorrectSound.volume = .55;
+const resultsSound = document.getElementById("results");
+const bgm = document.getElementById("backgroundMusic");
+bgm.volume = .2;
+const failed = document.getElementById("failed");
+
+let currentQuestionIndex = 0;
+let totalQuestions = 10;
+let score = 0;
+let currentLanguage = 'en';
+
+
+//sets the elements for the start of the quiz
+function startQuiz(){
+    currentQuestionIndex = 0;
+    document.getElementById('progressBar').style.width = 0 + '%';
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+//toggles mute for the background music playing
+function toggleMute(){
+    const music = document.getElementById("backgroundMusic");
+    const button = document.getElementById("muteButton");
+
+    if(music.muted){
+        music.muted = false;
+        button.textContent = "🔇";
+    } else {
+        music.muted = true;
+        button.textContent = "🔊";
+    }
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + 
+    currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+//updates progress bar based on current progress
+function updateProgress(){
+    const progressPercent = ((currentQuestionIndex+1) / totalQuestions) * 100;
+    document.getElementById('progressBar').style.width = progressPercent + '%';
+}
+
+//logic for when a question is correctly or incorrectly chosen
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        correctSound.currentTime = 0.95;
+        correctSound.play();
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        incorrectSound.currentTime = 0.3;
+        incorrectSound.play();
+        selectedBtn.classList.add("incorrect");
+    }
+    updateProgress();
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = "true";
+    });
+    nextButton.style.display = "block";
+}
+
+//translate questions to Spanish and vice versa
+   /* document.getElementById("lang").addEventListener("click", async () => {
+        if (currentLanguage === 'en') {
+          // Switch to Spanish
+          questions = randomSortArrays(questions0).slice(0,10);
+          currentLanguage = 'es';
+          document.getElementById("lang").textContent = "English";
+        } else {
+          // Switch to English
+          renderQuestions(questions);
+          currentLanguage = 'en';
+          document.getElementById("lang").textContent = "Spanish";
+        }
+      }); */
+
+// translates the questions
+    async function translateQuestions(questions, targetLanguage = 'es'){
+        const translatedQuestions = await Promise.all(questions.map(async (questionObj) => {
+            const translatedQuestion = await translateText(questionObj.question, targetLanguage);
+            return { ...questionObj, question: translatedQuestion };
+        }));
+
+        return translatedQuestions;
+    }
+
+// translate text
+    async function translateText(text, targetLanguage){
+        const response = await fetch("https://libretranslate.com/translate",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                q: text,
+                source: "en",
+                target: targetLanguage,
+                format: "text"
+            })
+        });
+
+        const data = await response.json();
+        return data.translatedText;
+    }
+
+//compute final score and play/show appropriate visuals and audio for high or low score
+function showScore(){
+    resetState();
+    final = ((score / questions.length)*100) + '%';
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!  ${final}! `;
+    if(((score/questions.length)*100) > 50){
+        nextButton.innerHTML = "Play Again";
+        resultsSound.play();
+        document.getElementById("popUp").style.display = "block";
+        document.getElementById("popUp1").style.display = "block";
+    } else {
+        nextButton.innerHTML = "Try Again";
+        failed.currentTime = 1.5;
+        failed.play();
+        document.getElementById("popUp3").style.display = "block";
+        document.getElementById("popUp2").style.display = "block";
+    }
+    nextButton.style.display = "block";
+    
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        questions = randomSortArrays(questions0).slice(0,10);
+        document.getElementById("popUp").style.display = "none";
+        document.getElementById("popUp1").style.display = "none";
+        document.getElementById("popUp3").style.display = "none";
+        document.getElementById("popUp2").style.display = "none";
+        startQuiz();
+    }
+})
+
+startQuiz();
+
+//add Spanish translations
+ 
